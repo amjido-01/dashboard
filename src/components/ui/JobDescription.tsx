a9e3947dd55e8b555c3d61d13051fb8ebc5ebcf9
amjido-01 <youndsadeeq10@gmai;.com>
2024-06-27 21:21:12 +0100

    commit

--- src/components/ui/JobDescription.tsx
+++ src/components/ui/JobDescription.tsx
@@ -8,7 +8,7 @@ const JobDescription = () => {
   return (
     <div className="px-[1rem] md:container md:pr-0 flex flex-col md:flex-row"> 
       <Preview />
-    <div className="w-full hidden md:block md:w-[25%] bg-[#FCFCFC] p-[18px] border-l-[1px] border-t-[1px] border-[#E7E7E7]">
+    <div className="w-full hidden md:block md:w-[25%] bg-[#FCFCFC] px-[18px] py-[26px] border-l-[1px] border-t-[1px] border-[#E7E7E7]">
       <Aside />           
       </div>
   </div>
